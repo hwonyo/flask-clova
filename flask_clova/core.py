1caf79b0a11e282add8772c22d0eacb0f1235bc3
wonyoHwang <hollal0726@gmail.com>
2018-05-28 14:59:38 +0900

    Add Test Mode

--- flask_clova/core.py
+++ flask_clova/core.py
@@ -110,6 +110,13 @@ class Clova(object):
             It is useful for mocking JSON requests in automated tests.
             Default: True
 
+        `COLVA_TEST_MODE`:
+
+            Enables or disables CEK test mode, which fills missing field during test
+            specifically sessionAttributes. This setting should not be abled in production.
+            It is usefule when test with CEK console.
+            Default: False
+
         `CLOVA_PRETTY_DEBUG_LOGS`:
 
             Add tabs and linebreaks to the CEK request and response printed to the debug log.
@@ -146,12 +153,16 @@ class Clova(object):
         blueprint.jinja_loader = ChoiceLoader([YamlLoader(blueprint, path)])
 
     @property
+    def clova_application_id(self):
+        return current_app.config.get('CLOVA_APPLICATION_ID', None)
+
+    @property
     def clova_verify_requests(self):
         return current_app.config.get('CLOVA_VERIFY_REQUESTS', True)
 
     @property
-    def clova_application_id(self):
-        return current_app.config.get('CLOVA_APPLICATION_ID', None)
+    def clova_test_mode(self):
+        return current_app.config.get('COLVA_TEST_MODE', False)
 
     def on_session_started(self, f):
         """Decorator to call wrapped function upon starting a session.
@@ -297,7 +308,7 @@ class Clova(object):
             return self.context.get('System', {}).get('user', {}).get('userId')
         return None
 
-    def _cek_request(self, verify=True):
+    def _cek_request(self, verify=True, test_mode=False):
         raw_body = flask_request.data
         cek_request_payload = json.loads(raw_body)
 
@@ -307,10 +318,14 @@ class Clova(object):
                 application_id = cek_request_payload['context']['System']['application']['applicationId']
                 verifier.verify_application_id(application_id, self.clova_application_id)
 
+        if test_mode:
+            if session.sessionAttributes is None:
+                session.sessionAttributes = {}
+
         return cek_request_payload
 
     def _flask_view_func(self, *args, **kwargs):
-        clova_payload = self._cek_request(verify=self.clova_verify_requests)
+        clova_payload = self._cek_request(verify=self.clova_verify_requests, test_mode=self.clova_test_mode)
         dbgdump(clova_payload)
         request_body = models._Field(clova_payload)
 
