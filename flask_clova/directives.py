0d667fe27cf80f63d80a15c9d6b5dd24e59fde24
jhlee7 <leejaeh0623@gmail.com>
2021-04-23 15:38:11 +0900

    Add emoji directive

--- flask_clova/directives.py
+++ flask_clova/directives.py
@@ -10,3 +10,41 @@ class directive:
                 "explicit": True
             }
         }
+
+    @staticmethod
+    def emoji(label):
+        """
+        - Emoji directive for Briefing-ai's sentiment analysis api spec
+        https://oss.navercorp.com/briefing-ai/briefing-ai-docs/wiki/감정-분석-한국어-API
+
+        Args:
+            label: 0(neutral), 1(positive), 2(negative)
+
+        Returns:emoji character corresponding label
+        """
+
+        emoji_chars = ["U+1F610", "U+1F600", "U+1F61E"]  # neutral, happy, sad
+
+        return {
+            "header": {
+                "name": "RenderTemplate",
+                "namespace": "Clova"
+            },
+            "payload": {
+                "emoji": {
+                    "type": "string",
+                    "value": emoji_chars[label]
+                },
+                "failureMessage": {
+                    "type": "string",
+                    "value": ""
+                },
+                "meta": {
+                    "version": {
+                        "type": "string",
+                        "value": ""
+                    }
+                },
+                "type": "TomorrowWeather"
+            }
+        }
